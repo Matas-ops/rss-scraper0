443443d2a475a9d20c071b6546ace7a17f11fed5
Matas-ops <matukas.jurevicius@gmail.com>
2026-01-01 18:00:49 +0200

    add CategoryMap.cs

---
+++ Models/CategoryMap.cs
@@ -0,0 +1,20 @@
+namespace BnsNewsRss.Models;
+
+public class CategoryMap
+{
+    public Dictionary<string[], string> Bns = new()
+    {
+        //compared as starts with
+        //from -> to
+        {["ES", "Krašto apsauga", "Politika", "Savivalda", "Švietimas", "Tarptaut.", "Teisėsauga"], "Aktualijos" },
+        {["Energetika", "ITT", "Maisto pramonė", "NT", "Pramonė", "Prekyba", "Socialinė sauga", "Transportas", "Turizmas", "Žiniasklaida", "Verslas", "Žemės ūkis"], "Verslas" },
+        {["Ekologija", "Gamta"], "Margumynai" },
+        //{[], "Nuomonės" },
+        {["Kultūra", "Laisvalaikis"], "Pramogos" },
+        {["Sportas"], "Sportas" },
+        {["Medicina", "Sveikata"], "Sveikata" },
+        {["ITT"], "Technologijos" },
+        {["Energetika", "Maisto pramonė", "NT", "Pramonė", "Prekyba", "Socialinė sauga", "Turizmas", "Verslas", "Žemės ūkis", "Žiniasklaida"], "Ekonomika" },
+        {["Transportas"], "Transportas" },
+    };
+}
