bab1b4a04006411e1db4c07a1015d0589fa9ffbe
Matas-ops <matukas.jurevicius@gmail.com>
2026-01-01 17:10:50 +0200

    no author

--- Services/RssAggregatorService.cs
+++ Services/RssAggregatorService.cs
@@ -106,7 +106,7 @@ public class RssAggregatorService
             {
                 scrapedItems.Add(item with
                 {
-                    Content = $"{scraped.Content} <p><a href=\"{item.Link}\">Originalaus straipsnio nuoroda</a></p>",
+                    Content = scraped.Content,
                     FeaturedImage = scraped.FeaturedImage ?? GetFeaturedImageFromDescription(item.Description)
                 });
             }
