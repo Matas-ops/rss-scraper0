51d73636ef4c4723c150e1d8283ed43508ca555e
Matas-ops <matukas.jurevicius@gmail.com>
2026-01-06 23:26:55 +0200

    remove unused field

--- Keys/CacheKeys.cs
+++ Keys/CacheKeys.cs
@@ -3,7 +3,6 @@ namespace BnsNewsRss.Keys;
 public static class CacheKeys
 {
     public const string WordPressFeed = "wordpress_feed_xml";
-    public const string SeenGuids = "seen_guids";
 
     public static string Article(string guid) => $"article::{guid}";
 }
